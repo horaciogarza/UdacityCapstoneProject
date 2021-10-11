36cb12aa81e86a003fa56c26531469786c1cf7fa
Horacio Garza <hgx95@hotmail.com>
2021-10-11 10:23:59 -0500

    Add files via upload

--- etl_process.py
+++ etl_process.py
@@ -23,7 +23,7 @@ def main():
     #Revoking access to avoid table block from users
     cur.execute(sp_revoke_access)
 
-    process_coredata_file(cur,filepath)
+    process_coredata_file(cur, filepath, conn)
     process_data_cleansing(cur)
 
     #Granting access again
@@ -36,7 +36,7 @@ def main():
 
 
 
-def process_coredata_file(cur, filepath):
+def process_coredata_file(cur, filepath, conn):
     """
         process_coredata_file Function:
         PARAMETERS:
@@ -56,7 +56,12 @@ def process_coredata_file(cur, filepath):
 
     
 
-    #df2 = df.iloc[:1000]
+    
+
+    #getting the count before inserting values for unit test purposes
+    cur.execute(count_of_coretable)
+    result = cur.fetchone()
+    countBeforeInserting = result[0]
 
     #Deleting before inserting
     cur.execute(coredata_table_delete)
@@ -64,11 +69,34 @@ def process_coredata_file(cur, filepath):
 
     print("Uploading data")
 
-    for index, row in df.iterrows():
+    totalRows = len(df.index)
+    totalRowsInserted = 0
+    countAfterInserting = 0
+    countBeforeInserting = 0
+
+   
+
+    df2 = df.iloc[:20]
+
+    for index, row in df2.iterrows():
         print("Inserting row ",index+1)
 
         data_to_upload = [row.FECHA_ACTUALIZACION, row.ID_REGISTRO,   row.ORIGEN,    row.SECTOR,    row.ENTIDAD_UM,    row.SEXO,  row.ENTIDAD_NAC,   row.ENTIDAD_RES,   row.MUNICIPIO_RES, row.TIPO_PACIENTE, row.FECHA_INGRESO, row.FECHA_SINTOMAS,    row.FECHA_DEF, row.INTUBADO,  row.NEUMONIA,  row.EDAD,  row.NACIONALIDAD,  row.EMBARAZO,  row.HABLA_LENGUA_INDIG,    row.INDIGENA,  row.DIABETES,  row.EPOC,  row.ASMA,  row.INMUSUPR,  row.HIPERTENSION,  row.OTRA_COM,  row.CARDIOVASCULAR,    row.OBESIDAD,  row.RENAL_CRONICA, row.TABAQUISMO,    row.OTRO_CASO, row.TOMA_MUESTRA_LAB,  row.RESULTADO_LAB, row.TOMA_MUESTRA_ANTIGENO, row.RESULTADO_ANTIGENO,    row.CLASIFICACION_FINAL,   row.MIGRANTE,  row.PAIS_NACIONALIDAD, row.PAIS_ORIGEN,row.UCI]
         cur.execute(coredata_table_insert, data_to_upload)
+        totalRowsInserted = index+1
+
+
+
+    conn.commit()
+
+    #getting the count AFTER inserting values for unit test purposes
+    cur.execute(count_of_coretable)
+    result2 = cur.fetchone()
+    countAfterInserting = result2[0]
+    
+
+    controlTableValues = [countBeforeInserting, totalRows, totalRowsInserted, countAfterInserting]
+    cur.execute(control_table_insert, controlTableValues)
 
     print("Data uploaded")
     
